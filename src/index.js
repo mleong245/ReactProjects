d36f8f48631583323d5043f44b7685328088ef2b
Matthew Leong <matthew.leong@workday.com>
2017-04-14 12:24:30 -0700

    Up through section 2, lecture 31
    Implemented a lot of the base functionality for the simple YouTube player.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-import config from './config'
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import config from './config';
 const API_KEY = config.YOUTUBE_API_KEY;
 
 // Create a new component. This component should produce
 // some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar /> // created an instance of SearchBar
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('basketball');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it
